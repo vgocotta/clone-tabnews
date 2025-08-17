fb78af8cae625fd315e8cf2e98db86669370c9ac
vgocotta <46388206+vgocotta@users.noreply.github.com>
2025-08-17 23:37:23 +0000

    modificar mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Anabel, você é uma mulher maravilhosa. É muito bom amar você!</h1>
+    return <h1>Anabel, você é uma mulher maravilhosa. É muito bom amar você! Vem aqui me dar um abraço assim que ver isso.</h1>
 }
 
 export default Home;
